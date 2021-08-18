fd8132d6a1633c0b3384a6a5c05e271491a82f17
theo.atlani@gmail.com <theo.atlani@gmail.com>
2021-08-18 14:33:06 +0200

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
